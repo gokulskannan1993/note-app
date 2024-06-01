7cab8872e49b42c8f6a973a2bc11f00a75181ee1
Gokul Kannan <gokulskannan1993@gmail.com>
2024-06-01 22:09:35 +0100

    inputs created

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gokulskannan1993/note
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+
+	title, content, err := getNoteData()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
+
+func getNoteData() (string, string, error) {
+	title, err := getUserInput(("Note title"))
+
+	if err != nil {
+		return "", "", err
+	}
+
+	content, err := getUserInput("Note Content")
+
+	if err != nil {
+		return "", "", err
+	}
+
+	return title, content, nil
+}
+
+func getUserInput(prompt string) (string, error) {
+	fmt.Print(prompt)
+	var value string
+
+	fmt.Scanln(&value)
+
+	if value == "" {
+		return "", errors.New(("Invalid Input"))
+	}
+
+	return value, nil
+}
