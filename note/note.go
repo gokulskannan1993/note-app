0afc32b7598b2827670d1a2ee34e66bb7a3a0951
Gokul Kannan <gokulskannan1993@gmail.com>
2024-06-01 22:14:44 +0100

    created a basic note struct

---
+++ note/note.go
@@ -0,0 +1,18 @@
+package note
+
+import "time"
+
+type Note struct {
+	title     string
+	content   string
+	createdAt time.Time
+}
+
+func New(title, content string) Note {
+
+	return Note{
+		title:     title,
+		content:   content,
+		createdAt: time.Now(),
+	}
+}
